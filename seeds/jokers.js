a22b5d4695fb17b3ee03b87d20c9415962ebab8f
rosedhlakama <rosejembe@live.com>
2019-08-23 10:51:24 +1200

    add joker table

--- seeds/jokers.js
+++ seeds/jokers.js
@@ -1,15 +1,10 @@
 
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
+    return knex('jokers').insert([
         {id: 88801, joker: 'Nic Matthews', about: 'funny'},
         {id: 88802, joker: 'July Seng', about: 'so so funny'},
         {id: 88803, joker: 'Sylvia Gilbert-Potts', about: 'oh my god hilarious'},
         {id: 88804, joker: 'Rose Dhlakama', about: 'terribly funny'}
 
       ]);
-    });
 };
