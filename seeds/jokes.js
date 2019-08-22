cdad3d05dd67903c0329eb1688fd9df3c6fc623e
student <student@pop-os.localdomain>
2019-08-22 17:23:31 +1200

    fixed typo

--- seeds/jokes.js
+++ seeds/jokes.js
@@ -1,6 +1,6 @@
 exports.seed = function (knex, Promise) {
   return knex('jokes').insert([
-    { id: 99901, name: 'Nic Matthews', joke: 'Why did a crow win an award? Because he was outstanding in his field.'},
+    { id: 99901, name: 'Nic Matthews', joke: 'Why did the scarecrow win an award? Because he was outstanding in his field.'},
     { id: 99902, name: 'July  Seng', joke: 'What did the giant say after he ate Tonga?...I want Samoa' },
     { id: 99903, name: 'Rose Dhlakama', joke: 'The shovel was a ground breaking invention.' },
     { id: 99904, name: 'Sylvia Gilbert-Potts', joke: 'A dyslexic guy walks into a bra' },
