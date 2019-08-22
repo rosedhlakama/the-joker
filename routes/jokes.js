a6d445549e307ad141d4a18bfa12737c5122b79c
Sylvia Gilbert-Potts <51007051+sylvia-gp@users.noreply.github.com>
2019-08-23 11:33:26 +1200

    accept changes from new branch

--- routes/jokes.js
+++ routes/jokes.js
@@ -34,10 +34,6 @@ router.get('/profile/:id', (req, res) =>{
       })
     })
 })
-<<<<<<< HEAD
-=======
-
->>>>>>> 1c55fc17ee36a56637817247b79c6871de570f36
 router.get('/addJoke', (req, res) => {  
   res.render('./addJoke')
 })
